c98653aec81ccd5d97730b336dc9d579f0728b9b
unknown <bettkipngenobrian@gmail.com>
2022-10-31 15:10:16 +0300

    Edit Phone Fragment

---
+++ app/src/main/java/com/example/techmall/activities/AddItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.techmall.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.techmall.R
+
+class AddItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_item)
+    }
+}
